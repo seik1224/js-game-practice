e4f5dbe3119e6edee6660a1c748f65add81ac721
Jeongro Seok <jeongroseok@gmail.com>
2020-07-20 00:03:47 +0900

    셋팅

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+};
